a1f1ba4217c4236650b11bc77b2da8407f3ccacd
Petr Korolev <sky4winder@gmail.com>
2021-04-23 17:02:34 +0700

    change email @vas3k.club -> @oxor.io

--- README.md
+++ README.md
@@ -135,6 +135,6 @@ Take some time to press F and give some respects to our [best contributors](http
 
 In other words, you can use the code for private and commercial purposes with an author attribution (by including the original license file or mentioning the Club 🎩).
 
-Feel free to contact us via email [club@vas3k.club](mailto:club@vas3k.club).
+Feel free to contact us via email [club@oxor.io](mailto:club@oxor.io).
 
 ❤️
--- club/settings.py
+++ club/settings.py
@@ -207,7 +207,7 @@ JWT_EXP_TIMEDELTA = timedelta(days=120)
 
 MAILGUN_API_URI = "https://api.eu.mailgun.net/v3/mailgun.vas3k.club"
 MAILGUN_API_KEY = os.getenv("MAILGUN_API_KEY")
-MAILGUN_EMAIL_FROM = "Вастрик.Клуб <club@vas3k.club>"
+MAILGUN_EMAIL_FROM = "Вастрик.Клуб <club@oxor.io>"
 
 MEDIA_UPLOAD_URL = "https://i.phangan.me/upload/multipart/"
 MEDIA_UPLOAD_CODE = os.getenv("MEDIA_UPLOAD_CODE")
@@ -247,7 +247,7 @@ POST_HOTNESS_PERIOD = timedelta(days=5)
 MAX_COMMENTS_FOR_DELETE_VS_CLEAR = 10
 CLEARED_POST_TEXT = "```\n" \
     "😥 Этот пост был удален самим автором и от него остались лишь комментарии участников. " \
-    "Если вы хотите приютить и развить эту тему как новый автор, напишите модераторам Клуба: moderator@vas3k.club." \
+    "Если вы хотите приютить и развить эту тему как новый автор, напишите модераторам Клуба: moderator@oxor.io." \
     "\n```"
 
 MODERATOR_USERNAME = "moderator"
