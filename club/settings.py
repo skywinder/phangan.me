a0c526584e268a9b3523054cbd9698dc87bf60c2
Petr Korolev <sky4winder@gmail.com>
2021-04-23 17:04:17 +0700

    Вастрик.Клуб -> Панган.Клуб

--- club/settings.py
+++ club/settings.py
@@ -152,7 +152,7 @@ LANDING_CACHE_TIMEOUT = 60 * 60 * 24
 # App
 
 APP_HOST = os.environ.get("APP_HOST") or "http://127.0.0.1:8000"
-APP_NAME = "Вастрик.Клуб"
+APP_NAME = "Панган.Клуб"
 APP_DESCRIPTION = "Всё интересное происходит за закрытыми дверями"
 LAUNCH_DATE = datetime(2020, 4, 13)
 
@@ -207,7 +207,7 @@ JWT_EXP_TIMEDELTA = timedelta(days=120)
 
 MAILGUN_API_URI = "https://api.eu.mailgun.net/v3/mailgun.vas3k.club"
 MAILGUN_API_KEY = os.getenv("MAILGUN_API_KEY")
-MAILGUN_EMAIL_FROM = "Вастрик.Клуб <club@oxor.io>"
+MAILGUN_EMAIL_FROM = "Панган.Клуб <club@oxor.io>"
 
 MEDIA_UPLOAD_URL = "https://i.phangan.me/upload/multipart/"
 MEDIA_UPLOAD_CODE = os.getenv("MEDIA_UPLOAD_CODE")
