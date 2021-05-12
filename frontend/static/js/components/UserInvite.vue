f67750db5147e7b2defbc0359bf0e7dd3295ae85
Svetlana Esina <magistresina@gmail.com>
2021-05-12 13:07:38 +0700

    Refactor user invites page: create vue template and style files, change content

---
+++ frontend/static/js/components/UserInvite.vue
@@ -0,0 +1,40 @@
+<template>
+    <li @click="copyInvite">
+        <label class="copy-invite-item">
+            <input type="text" :value="inviteUrl" ref="input" readonly>
+            <button class="button" title="Копировать адрес инвайта">
+                Copy
+            </button>
+        </label>
+    </li>
+</template>
+
+<script>
+export default {
+    name: "PostUpvote",
+    props: {
+        url: {
+            type: String,
+            default: '',
+            required: true,
+        },
+        days: {
+            type: Number,
+            default: 0,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            inviteUrl: window.location.origin + this.url,
+            inviteDays: this.days || '',
+        };
+    },
+    methods: {
+        copyInvite() {
+            this.$refs.input.select();
+            document.execCommand('copy');
+        },
+    },
+};
+</script>
