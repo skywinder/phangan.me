3fcd063afc0f4b022e0b5826993d157ba787b8a8
idealatom <idealatom@gmail.com>
2021-05-17 15:54:30 +0700

    add "publish" button on post preview page

--- club/urls.py
+++ club/urls.py
@@ -22,7 +22,7 @@ from posts.views.admin import admin_post, announce_post
 from posts.views.api import toggle_post_bookmark
 from posts.views.feed import feed
 from posts.views.posts import show_post, edit_post, upvote_post, retract_post_vote, compose, compose_type, \
-    toggle_post_subscription, delete_post, unpublish_post, clear_post
+    toggle_post_subscription, delete_post, publish_post, unpublish_post, clear_post
 from bookmarks.views import bookmarks
 from search.views import search
 from users.api import api_profile
@@ -90,6 +90,7 @@ urlpatterns = [
     path("post/<slug:post_slug>/clear/", clear_post, name="clear_post"),
     path("post/<slug:post_slug>/delete/", delete_post, name="delete_post"),
     path("post/<slug:post_slug>/edit/", edit_post, name="edit_post"),
+    path("post/<slug:post_slug>/publish/", publish_post, name="publish_post"),
     path("post/<slug:post_slug>/bookmark/", toggle_post_bookmark, name="toggle_post_bookmark"),
     path("post/<slug:post_slug>/upvote/", upvote_post, name="upvote_post"),
     path("post/<slug:post_slug>/retract_vote/", retract_post_vote, name="retract_post_vote"),
